cf0374ae23cbb3cafe60d9b424ade46721d502c5
bhanu korremula <bkraider@gmail.com>
2018-03-30 19:00:52 -0400

    practicing software fundamentals

--- softwarefundamentals/Program.cs
+++ softwarefundamentals/Program.cs
@@ -10,6 +10,27 @@ namespace softwarefundamentals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the ")
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonnaci number is:" + Fibonacci(n));
+            Console.ReadLine();
+        }
+
+
+        public static int Fibonacci(int n)
+        {
+            if(n <= 0)
+            {
+                return 0;
+            }
+            else if(n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
         }
     }
 }
